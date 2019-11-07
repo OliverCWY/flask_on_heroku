fc256af4ef3ff41b9ed4415d6d9c252eb57909f6
OliverCWY <chen.wuyang@dhs.sg>
2019-11-07 14:46:36 +0800

    a

--- app.py
+++ app.py
@@ -1,30 +1,6 @@
-import requests
-from urllib.parse import urlencode
-def get_response(msg):
-    key={
-        "key":"free",
-        "appid":0,
-        "msg":"你好"
-    }
-    response=requests.get("http://api.qingyunke.com/api.php?"+urlencode(key)).json()
-    return None if response["result"] else response["content"]
-
 from flask import Flask,render_template,request,make_response
 from flask_cors import cross_origin
-import requests
 app = Flask(__name__)
-
-@app.route('/qyk')
-@cross_origin(origins="*")
-def qyk():
-    inp=list(request.args)[0]
-    return get_response(inp)
-
-@app.route('/cors')
-@cross_origin(origins="*")
-def cors():
-    inp=list(request.args)[0]
-    return requests.get(inp).text
 
 @app.route('/')
 @cross_origin(origins="*")
